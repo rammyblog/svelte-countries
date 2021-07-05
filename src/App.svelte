dcc0f340249f0e60f0fcbc653ea4472a0cb2cb89
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-07-05 18:54:25 +0100

    Added routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,19 @@
 <script>
   // Import
   import Navbar from "./components/Navbar.svelte";
-  import { Router, Route, Link } from "svelte-navigator";
+  import { Router, Route } from "svelte-routing";
   import Home from "./components/Home.svelte";
+  import Country from "./components/Country.svelte";
+  export let url = ""; //This property is necessary declare to avoid ignore the Router
 </script>
 
 <Navbar />
 <div class="main">
-  <Router primary={false}>
+  <Router {url}>
     <Route path="/" component={Home} />
+    <Route path="country/:name" let:params>
+      <Country name={params.name} />
+    </Route>
   </Router>
 </div>
 
---
+++ src/components/Country.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let name;
+  console.log(name);
+</script>
+
+<div>hELLO WORLD</div>
+
+<style></style>
